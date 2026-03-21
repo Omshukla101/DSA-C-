825dd15fa412ad0f6a0d0b7c5909c6d535a9ddb6
Om Shukla <oms20405@gmail.com>
2026-03-21 13:00:41 +0530

    Add files via upload

---
+++ pettern/pattern9.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	
+	for(int i=1;i<=5;i++){
+		char ch='A'+i-1;
+	  for(int j=1;j<=5;j++){
+			cout<<ch;
+		}
+		cout<<endl;
+		
+	}
+	return 0;
+}
